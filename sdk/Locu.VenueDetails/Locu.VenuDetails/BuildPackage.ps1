8779a442e6d7e5e9d11f867acc82dd1aa6bb7821
adam grocholski <adam@thinkfirstcodelater.com>
2014-01-23 14:15:33 -0600

    fixed nuspec and added packaging scripts

---
+++ sdk/Locu.VenueDetails/Locu.VenuDetails/BuildPackage.ps1
@@ -0,0 +1,3 @@
+﻿if (!(test-path "c:\code\nuget")){[IO.Directory]::CreateDirectory("c:\code\nuget")}
+
+nuget pack Locu.VenueDetails.csproj -OutputDirectory "c:\code\nuget" -Build
---
+++ sdk/Locu.VenueSearch/Locu.VenueSearch/BuildPackage.ps1
@@ -0,0 +1,3 @@
+﻿if (!(test-path "c:\code\nuget")){[IO.Directory]::CreateDirectory("c:\code\nuget")}
+
+nuget pack Locu.VenueSearch.csproj -OutputDirectory "c:\code\nuget" -Build
