d91002c8e473c3a8c38effd2f84fe227fee676a2
adam grocholski <adam@thinkfirstcodelater.com>
2014-01-23 18:37:42 -0600

    added readme for info on running, customizing, and submitting

--- samples/windows-phone-8/SingleVenue/SingleVenue/Resources/Keys.cs
+++ samples/windows-phone-8/SingleVenue/SingleVenue/Resources/Keys.cs
@@ -8,20 +8,6 @@ namespace SingleVenue.Resources
 {
     public class Keys
     {
-        //TODO: Get an ApplicationID and AuthenticationToken from the Dev Center
-        /*
-         * 1. Get a Locu API key at https://dev.locu.com/. Use your key for the value of the LocuApiKey property.
-         * 2. Obtain the Locu Id of the venue you want to build the app for. Use the id for the value of teh LocuVenuId property.
-         *      Note: You can either use the Locu developer web console (https://dev.locu.com/console/) or 
-         *      the Locu venue details console that is part of the Locu SDK for .NET (https://github.com/agrocholski/locu-sdk-for-net).
-         * 3. After you have finished your app, begin the app submission process to submit it to the Windows Phone store.
-         * 4. On the SUBMIT APP page, click MAP SERVICES
-         * 5. On the page click, GET TOKEN
-         *    The new ApplicationId and AuthenticationToken are displayed on the same page
-         * 7. Copy the ApplicationId and AuthenticationToken values and paste them into the corresponding properties below.
-         * 8. Rebuild your app with the new code and upload to the Store.
-         * */
-
         public static string LocuApiKey
         {
             get { return ""; }
