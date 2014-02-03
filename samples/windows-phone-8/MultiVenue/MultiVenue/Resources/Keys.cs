1dee62c6fc66b4c3a1ab308ef4e8899b0d12269f
adam grocholski <adam@thinkfirstcodelater.com>
2014-02-03 13:06:33 -0600

    assets

---
+++ samples/windows-phone-8/MultiVenue/MultiVenue/Resources/Keys.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiVenue.Resources
+{
+    public class Keys
+    {
+        public static string LocuApiKey
+        {
+            get { return ""; }
+        }
+
+        public static List<string> LocuVenueIds
+        {
+            get { return new List<string>() { "" }; }
+        }
+
+        public static string MapsApplicationId
+        {
+            get { return ""; }
+        }
+
+        public static string MapsAuthenticationToken
+        {
+            get { return ""; }
+        }
+    }
+}
